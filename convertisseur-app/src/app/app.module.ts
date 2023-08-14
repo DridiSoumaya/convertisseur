49b208ba00a585f2899b469a6083180c319a965c
soumaya <soumaya.dridi1@gmail.com>
2023-08-14 12:14:01 +0200

    ajout taux de change aleatoire et switch EUR/USD

--- convertisseur-app/src/app/app.module.ts
+++ convertisseur-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { ConvertisseurDeviseComponent } from './convertisseur-devise/convertisse
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- convertisseur-app/src/app/convertisseur-devise/convertisseur-devise.component.ts
+++ convertisseur-app/src/app/convertisseur-devise/convertisseur-devise.component.ts
@@ -7,8 +7,44 @@ import { Component, OnInit} from '@angular/core';
 })
 export class ConvertisseurDeviseComponent implements OnInit{
 
+  montant: number = 0;
+  tauxChange: number = 1.1;
+  montantConverti: number = 0;
+  devise: string = 'EUR';
+  autreDevise: string = "USD";
+  manualTaux: number = 0;
+  
+
+
 
   ngOnInit(): void {
-   
+    this.initTauxChange();
+    setInterval(() => {
+      this.updateTauxChange();
+    }, 3000);
   }
+
+  /* initialiser taux de change EUR / USD a 1.1 */
+  initTauxChange(): void {
+    this.tauxChange = 1.1;
+  }
+  /*update taux de change a random value between -0.05 and +0.05 s */
+  updateTauxChange(): void {
+    const variation = Math.random() * 0.1 - 0.05;
+    this.tauxChange += variation;
+ 
+  }
+  /*convertir le montant selon le choix de devise */
+  convertirDevise(): void {
+    // this.montantConverti = this.montant * this.tauxChange;
+    if (this.devise === 'EUR') {
+      this.montantConverti = this.montant * this.tauxChange;
+      this.autreDevise = 'USD';
+    } else if (this.devise === 'USD') {
+      this.montantConverti = this.montant / this.tauxChange;
+      this.autreDevise = 'EUR';
+    }
+  
+  }
+  /** */
 }
